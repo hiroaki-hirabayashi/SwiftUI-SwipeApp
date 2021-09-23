14530c992b08dad7b51e2fa928f6f1f450d613b0
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-09-23 11:21:03 +0900

    Initial Commit

---
+++ SwiftUI-SwipeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SwipeApp
+//
+//  Created by 平林 宏淳 on 2021/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
