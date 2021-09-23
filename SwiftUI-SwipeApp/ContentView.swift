325773ba58aac6ae4be61350e0bd0df94e21b7c1
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-09-23 12:04:23 +0900

    update: 画面雛形作成

--- SwiftUI-SwipeApp/ContentView.swift
+++ SwiftUI-SwipeApp/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var frameWidth = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    // ボタンのイメージプロパティ
+                    Image(systemName: "flame.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.red)
+                })
+                // ボタンプロパティ
+                .frame(width: frameWidth / 4 - 20)
+                .padding(.leading, 30)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "suit.diamond.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.yellow)
+                })
+                .frame(width: frameWidth / 4)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "bubble.left.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.orange)
+                })
+                .frame(width: frameWidth / 4)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.purple)
+                })
+                .frame(width: frameWidth / 4 - 20)
+                .padding(.trailing, 30)
+                        
+            }
+            // Stackプロパティ
             .padding()
+            .frame(width: frameWidth)
+            
+            Text("Hello, world!")
+                .padding()
+                .frame(width: frameWidth)
+                .background(Color.blue)
+            Text("Hello, world!")
+                .padding()
+                .frame(width: frameWidth)
+                .background(Color.green)
+        }
     }
 }
 
